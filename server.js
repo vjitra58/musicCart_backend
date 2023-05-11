7b05a2a9e79ff56ee81e7c8cc92b8369b1c56cb0
vjitra58 <vikaskumar58616@gmail.com>
2023-05-11 18:48:24 +0530

    added port number

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
-
+const port = process.env.PORT || 5000;
 connectDB();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is working on port: ${process.env.PORT}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
