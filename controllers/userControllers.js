4dd7d7a82a1bd15bfe3087ed208694d8568c57a5
vjitra58 <vikaskumar58616@gmail.com>
2023-05-11 18:29:16 +0530

    error fixed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,10 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
-import ErrorHandler from "../utils/errorHandler.js";
+
 import { User } from "../models/UserModel.js";
 import { sendToken } from "../utils/sendToken.js";
 import crypto from "crypto";
 import { Product } from "../models/ProductModel.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 
 
